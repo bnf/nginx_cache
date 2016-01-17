ec5c1f9e79177a96699538857b7ea0a3069a2795
Benjamin Franzke <bfr@qbus.de>
2016-01-17 16:44:53 +0100

    Add a nginx cache backend to the caching framework
    It sends a X-Accel-Expires header when setting cache contents
    and clears using the nginx_purge command implemented in the
    previous commit.

---
+++ Classes/Cache/Backend/NginxCacheBackend.php
@@ -0,0 +1,91 @@
+<?php
+namespace Qbus\NginxCache\Cache\Backend;
+
+/**
+ * NginxCacheBackend
+ *
+ * @author Benjamin Franzke <bfr@qbus.de>
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class NginxCacheBackend extends \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend
+{
+    const CMD = 'typo3conf/ext/nginx_cache/Resources/Private/nginx_purge/nginx_purge';
+
+    /**
+     * Saves data in a cache file.
+     *
+     * @param string $entryIdentifier An identifier for this specific cache entry
+     * @param string $data The data to be stored
+     * @param array $tags Tags to associate with this cache entry
+     * @param int $lifetime Lifetime of this cache entry in seconds. If NULL is specified, the default lifetime is used. "0" means unlimited liftime.
+     * @return void
+     * @throws \TYPO3\CMS\Core\Cache\Exception if no cache frontend has been set.
+     * @throws \TYPO3\CMS\Core\Cache\Exception\InvalidDataException if the data to be stored is not a string.
+     */
+    public function set($entryIdentifier, $data, array $tags = array(), $lifetime = null)
+    {
+        parent::set($entryIdentifier, $data, $tags, $lifetime);
+
+        if ($lifetime === 0) {
+            // unlimited is not supported by nginx
+            $lifetime = 24*60*60;
+        }
+
+        header('X-Accel-Expires: ' . $lifetime);
+    }
+
+    /**
+     * Removes all cache entries matching the specified identifier.
+     *
+     * @param string $entryIdentifier Specifies the cache entry to remove
+     * @return bool TRUE if (at least) an entry could be removed or FALSE if no entry was found
+     */
+    public function remove($entryIdentifier)
+    {
+        $levels = '1:2';
+        $levels = explode(':', $levels);
+
+        $file = '';
+        $i = 0;
+        foreach ($levels as $level) {
+            $level = (int) $level;
+
+            $file .= substr($entryIdentifier, -($level + $i), $level) . '/';
+            $i += $level;
+        }
+
+        $file .= $entryIdentifier;
+        exec(PATH_site . self::CMD . ' ' . escapeshellarg($file));
+
+        return parent::remove($entryIdentifier);
+    }
+
+    /**
+     * Removes all cache entries of this cache.
+     *
+     * @return void
+     */
+    public function flush()
+    {
+        exec(PATH_site . self::CMD);
+
+        parent::flush();
+    }
+
+    /**
+     * Removes all cache entries of this cache which are tagged by the specified tag.
+     *
+     * @param string $tag The tag the entries must have
+     * @return void
+     */
+    public function flushByTag($tag)
+    {
+        $this->findIdentifiersByTag($tag);
+
+        $identifiers = $this->findIdentifiersByTag($tag);
+        foreach ($identifiers as $identifier) {
+            $this->remove($identifier);
+        }
+    }
+
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,12 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['nginx_cache'] = [
+    'frontend' =>  \TYPO3\CMS\Core\Cache\Frontend\StringFrontend::class,
+    'backend' => \Qbus\NginxCache\Cache\Backend\NginxCacheBackend::class,
+    'groups' => [
+        'pages',
+        'all'
+    ],
+];
