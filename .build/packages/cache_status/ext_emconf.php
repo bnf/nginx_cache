7322049d764a22322d722c0c0812500921c1f6f7
Benjamin Franzke <ben@bnf.dev>
2024-10-14 13:19:35 +0200

    Test against recent v13

--- .build/packages/cache_status/ext_emconf.php
+++ .build/packages/cache_status/ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.3.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '10.4.0-12.4.99',
+            'typo3' => '10.4.0-13.4.99',
         ),
         'conflicts' => array(
         ),
